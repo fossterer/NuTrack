844c1628575bd7d1376a079369e6ab33f861963e
fossterer <shashank@linux.com>
2019-11-04 23:59:52 -0500

    Basic calendar with onDateChangeListener added

---
+++ app/src/main/java/com/fossterer/nutrack/MainActivity.java
@@ -0,0 +1,26 @@
+package com.fossterer.nutrack;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String msg = "Date selected is " + dayOfMonth + "/" + (month + 1) + "/" + year;
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
